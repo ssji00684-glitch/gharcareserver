f5a6dd255ab8205f51ec4240e23cd3435b891224
ssji00684-glitch <ssji00684@gmail.com>
2025-11-09 17:57:09 +0530

    Delete authRoutes.js

--- authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'Auth route working successfully!' });
-});
-
-module.exports = router;

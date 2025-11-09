561f87ae1f2751dc68b282343c78f0e1c6f98578
ssji00684-glitch <ssji00684@gmail.com>
2025-11-09 17:59:57 +0530

    Delete smsRoutes.js

--- smsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendOtp, verifyOtp } = require('../controllers/smsController');
-
-router.post('/send', sendOtp);
-router.post('/verify', verifyOtp);
-
-module.exports = router;

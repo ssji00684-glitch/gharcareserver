3691afa60f7826e4057aca6845d55353bcb9cd8b
ssji00684-glitch <ssji00684@gmail.com>
2025-11-09 17:58:40 +0530

    Delete paymentRoutes.js

--- paymentRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express'); const router = express.Router();
-const { createOrder, verifyPayment } = require('../controllers/paymentController');
-router.post('/order', createOrder);
-router.post('/verify', verifyPayment);
-module.exports = router;

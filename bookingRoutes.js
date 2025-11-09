2a6d5d78ec733bc867321ee22191415a13f732a5
ssji00684-glitch <ssji00684@gmail.com>
2025-11-09 17:57:34 +0530

    Delete bookingRoutes.js

--- bookingRoutes.js
+++
@@ -1,8 +0,0 @@
-const { auth } = require('../utils/authMiddleware');
-const express = require('express'); const router = express.Router();
-const { create, byCustomer, all, updateStatus } = require('../controllers/bookingController');
-router.post('/', auth, create);
-router.get('/customer/:customerId', auth, byCustomer);
-router.get('/', all);
-router.patch('/:id/status', updateStatus);
-module.exports = router;

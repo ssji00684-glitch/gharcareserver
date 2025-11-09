bc8fdd38de1b895fe595ecfe607a8709c20c4cc1
ssji00684-glitch <ssji00684@gmail.com>
2025-11-09 18:00:41 +0530

    Delete userRoutes.js

--- userRoutes.js
+++
@@ -1,4 +0,0 @@
-const express = require('express'); const router = express.Router();
-const User = require('../models/User');
-router.get('/:id', async (req,res)=>{ const u = await User.findById(req.params.id); res.json({success:true,user:u}); });
-module.exports = router;

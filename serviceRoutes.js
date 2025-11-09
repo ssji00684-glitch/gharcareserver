a13c1b4a6136b0d9f9d9ad195ae843a1942d327d
ssji00684-glitch <ssji00684@gmail.com>
2025-11-09 17:59:31 +0530

    Delete serviceRoutes.js

--- serviceRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express'); const router = express.Router();
-const { addService, list } = require('../controllers/serviceController');
-router.post('/', addService);
-router.get('/', list);
-module.exports = router;
